2381b74beb9e8d24222d21f06e0592fdee610e7a
18678815113 <49201820+18678815113@users.noreply.github.com>
2020-03-17 16:54:58 +0800

    '项目'

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+const Home = () => import('../views/home/Home')
+const Cart = () => import('../views/cart/Cart')
+const Category = () => import('../views/category/Category')
+const Profile = () => import('../views/profile/Profile')
+//1.安装插件
+Vue.use(VueRouter)
+
+//2.创建router
+const routes=[
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/profile',
+    component: Profile
+  },
+]
+
+const router =new VueRouter({
+    routes,
+    mode:'history'
+})
+
+//导出路由
+export default router
